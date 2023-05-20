5b308e46927979ae2026b9f4f5a3c878e6741847
ArturT <mr.arturio@gmail.com>
2023-05-19 23:16:02 -0400

    Adding the Create and Edit Stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -153,24 +154,41 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => (
-  <Show 
-  student="Lydia Miller-Jones"
-  interviewer={interviewer}
-  onEdit={action('onEdit')}
-  onDelete={action('onDelete')}
-  />
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
   ))
-  .add("Confirm", () => ( 
-  <Confirm
-  message="Delete the appointment?"
-  onConfirm={action('onConfirm')}
-  onCancel={action('onCancel')}
-  />
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
   ))
-  .add("Status", () => <Status message="Deleting"/>)
+  .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => (
-  <Error
-  message="Could not delete appointment."
-  onClose={action('onClose')}
-  />
+    <Error
+      message="Could not delete appointment."
+      onClose={action('onClose')}
+    />
   ))
+  .add("Edit", () => (
+    <Form
+      student="Artur T"
+      interviewer="4"
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+
