25879f3ff53c9b9b47ec957b17b722e20067ab07
ArturT <mr.arturio@gmail.com>
2023-05-19 22:55:05 -0400

    add components: Header, Empty, Show, Confirm, Status, Error + stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,11 @@ import 'components/Appointment/styles.scss';
 
 import Header from "./Header";
 import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
 
   return (
     <article className="appointment"></article>
-  )
+  );
 }
