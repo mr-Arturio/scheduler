28ad014954fb0ef1a5da8c09b3286126f3756ec0
ArturT <mr.arturio@gmail.com>
2023-05-23 18:16:17 -0400

    add selectors + test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+
+  const filteredDay = state.days.find(d => d.name === day);
+
+  if (!filteredDay) {
+    return [];
+  }
+
+  return filteredDay.appointments.map((id) => state.appointments[id]);
+}
