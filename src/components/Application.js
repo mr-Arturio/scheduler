40e56743bd1593a3ad081753cc60f5e4fc02c56a
ArturT <mr.arturio@gmail.com>
2023-05-23 17:38:27 -0400

    Requesting the Days.remove hard coded data. add useEffect + axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
+import axios from "axios";
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,7 +46,20 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
+
+  useEffect(() => {
+    axios.get('/api/days') // Make the GET request to your API server
+      .then(response => {
+        setDays(response.data); // Set the days state with the response data
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  
   return (
     <main className="layout">
       <section className="sidebar">
