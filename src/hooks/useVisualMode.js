8225d5df27e78c76d9b0484028f11b1172cf67fe
ArturT <mr.arturio@gmail.com>
2023-05-25 17:48:02 -0400

    add comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,30 +2,40 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+  const [history, setHistory] = useState([initial]); //history state maintains a record of all previous modes.
 
-  function transition(newMode, replace = false) {
+  //The transition function updates the mode and history states
+  function transition(newMode, replace = false) { //replace parameter determines whether to replace the current mode in the history.
+    //update the mode state with the new mode value.
     setMode(newMode);
+    //takes the previous history as a parameter and returns the new history.
     setHistory((prevHistory) => {
       if (replace) {
+        //create a copy of the previous history array using spread
         const newHistory = [...prevHistory];
+        //remove the last mode from the new history
         newHistory.pop();
-        return [... newHistory, newMode];
-        
+        //add the new mode to the modified history array and returns it
+        return [...newHistory, newMode];
       }
+      //If replace is false, add the new mode to the existing history array and returns the updated history.
       return [...prevHistory, newMode]
     });
   };
 
+  // to go back to the previous mode in the history
   function back() {
+    //check the length
     if (history.length > 1) {
 
       setHistory((prevHistory) => {
+        //create a copy of the previous history array using the spread
         const newHistory = [...prevHistory];
+        //remove the last mode from the new history
         newHistory.pop();
-
+        //It retrieves the previous mode by accessing the last item in the updated history array.
         const prevItem = newHistory[newHistory.length - 1];
-
+        //sets the mode state to the previous mode.
         setMode(prevItem);
 
         return newHistory;
@@ -34,7 +44,7 @@ export default function useVisualMode(initial) {
     }
 
   }
-
+  // Returning the Hook's Values:
   return { mode, transition, back };
 }
 
