486c96465ecbe671f20c920983862e29a9c72366
ArturT <mr.arturio@gmail.com>
2023-06-02 20:00:45 -0400

    handle the case when the name or interviewer is missing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -27,6 +27,11 @@ export default function Appointment(props) {
 
   function save(name, interviewer) {
 
+    if (!name || !interviewer) {
+      transition(ERROR_SAVE, true);
+      return;
+    }
+
     const interview = {
       student: name,
       interviewer,
