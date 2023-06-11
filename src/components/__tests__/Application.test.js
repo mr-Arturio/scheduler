b5a5a802b0a5bed6d1b365c66a0e8baccf1b77d6
ArturT <mr.arturio@gmail.com>
2023-06-10 21:19:14 -0400

    integration testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,18 +4,47 @@ import {
   render, 
   cleanup, 
   fireEvent, 
-  waitForElement 
+  waitForElement,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  prettyDOM
 } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+describe("Application", () => {
+
+it("defaults to Monday and changes the schedule when a new day is selected", async () => { //using `asnc` to define the asynchronous function
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  await waitForElement(() => getByText("Monday"));//The Promise chain can be hidden by using the await keyword.
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
+
+
+it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  const { container } = render(<Application />);
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointments = getAllByTestId(container, "appointment");
+  const appointment = appointments[0];
+
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
   });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+
+  console.log(prettyDOM(appointment));
+});
 });
