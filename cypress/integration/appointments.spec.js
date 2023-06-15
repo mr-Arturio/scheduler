494f53a4a3ed293fbde6a2e70157637f3add3e9c
ArturT <mr.arturio@gmail.com>
2023-06-14 23:02:41 -0400

    Cypress Edit test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -30,6 +30,29 @@ describe("Appointments", () => {
     cy.get(".appointment__card--show").should("contain", "Sylvia Palmer");
 cy.contains(".appointment__card--show", "Lydia Miller-Jones");
   })
+
+  it("should edit an interview", () => {
+
+    //2. Clicks the edit button for the existing appointment
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+
+    //3. Changes the name and interviewer
+    cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Artur");
+    cy.get("[alt='Tori Malcolm']")
+    .click();
+
+    //4. Clicks the save button
+    cy.contains("Save")
+    .click();
+
+    //5. Sees the edit to the appointment
+    cy.get(".appointment__card--show").should("contain", "Artur");
+cy.contains(".appointment__card--show", "Tori Malcolm");
+  })
   
     
  
